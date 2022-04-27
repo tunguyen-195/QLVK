86511f158a6cdb80320cbb580848add4ab1eac57
hoanv51293 <58430422+hoanv51293@users.noreply.github.com>
2022-04-27 20:24:20 +0700

    Update lombok

--- src/main/java/com/qlvk/model/BaseModel.java
+++ src/main/java/com/qlvk/model/BaseModel.java
@@ -2,6 +2,11 @@ package com.qlvk.model;
 
 import java.sql.Timestamp;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BaseModel {
 	protected Timestamp cmnUpdateDate;
 	protected String primaryKey;
@@ -10,52 +15,4 @@ public class BaseModel {
 	protected String cmnCreateDateLabel;
 	protected String cmnUpdateUserLabel;
 	protected String cmnUpdateDateLabel;
-
-	public Timestamp getCmnUpdateDate() {
-		return cmnUpdateDate;
-	}
-
-	public void setCmnUpdateDate(Timestamp cmnUpdateDate) {
-		this.cmnUpdateDate = cmnUpdateDate;
-	}
-
-	public String getPrimaryKey() {
-		return primaryKey;
-	}
-
-	public void setPrimaryKey(String primaryKey) {
-		this.primaryKey = primaryKey;
-	}
-
-	public String getCmnCreateUserLabel() {
-		return cmnCreateUserLabel;
-	}
-
-	public void setCmnCreateUserLabel(String cmnCreateUserLabel) {
-		this.cmnCreateUserLabel = cmnCreateUserLabel;
-	}
-
-	public String getCmnCreateDateLabel() {
-		return cmnCreateDateLabel;
-	}
-
-	public void setCmnCreateDateLabel(String cmnCreateDateLabel) {
-		this.cmnCreateDateLabel = cmnCreateDateLabel;
-	}
-
-	public String getCmnUpdateUserLabel() {
-		return cmnUpdateUserLabel;
-	}
-
-	public void setCmnUpdateUserLabel(String cmnUpdateUserLabel) {
-		this.cmnUpdateUserLabel = cmnUpdateUserLabel;
-	}
-
-	public String getCmnUpdateDateLabel() {
-		return cmnUpdateDateLabel;
-	}
-
-	public void setCmnUpdateDateLabel(String cmnUpdateDateLabel) {
-		this.cmnUpdateDateLabel = cmnUpdateDateLabel;
-	}
 }
